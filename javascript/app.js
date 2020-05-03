a8cda629623d2c8970a88092d72d5e530f37891b
alayu <se.alayu.estifanos@gmail.com>
2020-05-03 22:09:44 +0300

    Creating the button scrolling and page navigation functionality

--- javascript/app.js
+++ javascript/app.js
@@ -35,3 +35,36 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
+  
+    console.log(e.target.getBoundingClientRect());
+  
+    console.log('Current scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+  
+    console.log(
+      'height/width viewport',
+      document.documentElement.clientHeight,
+      document.documentElement.clientWidth
+    );
+  
+    section1.scrollIntoView({ behavior: 'smooth' });
+  });
+  
+  ///////////////////////////////////////
+  // Page navigation
+  
+  
+  document.querySelector('.nav__links').addEventListener('click', function (e) {
+    e.preventDefault();
+  
+    // Matching strategy
+    if (e.target.classList.contains('nav__link')) {
+      const id = e.target.getAttribute('href');
+      document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+  
